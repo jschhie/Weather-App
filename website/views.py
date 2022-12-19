6eea7c044ac78084a7db13d13ad65093df84e225
Jennifer Chhie <jschhie@ucdavis.edu>
2022-12-18 20:28:00 -0800

    bootstrap cards, hero, searchbar

--- website/views.py
+++ website/views.py
@@ -6,18 +6,16 @@ from .models import City
 views = Blueprint('views', __name__)
 
 @views.route('/', methods=['GET', 'POST'])
-def index():
-    
+def home():
     if request.method == 'POST':
-        # get city name
-        new_city = request.form.get('city')
-        
-        if new_city:
-            # add to database   
-            new_city_obj = City(name=new_city)
-            db.session.add(new_city_obj)
-            db.session.commit()
-
+        # get city name from search bar
+        if request.form['action'] == 'search':
+            new_city = request.form['query']
+            if new_city:
+                # add to database   
+                new_city_obj = City(name=new_city)
+                db.session.add(new_city_obj)
+                db.session.commit()
     cities = City.query.all() # query all cities in table
 
     # q={} query is city name 
@@ -27,20 +25,17 @@ def index():
     weather_data = [] # list to hold all weather data per city
 
     for city in cities:
-
-        print(city.name)
-
         # send request to api where r = response
         r = requests.get(url.format(city.name)).json()
-    
         # create dictionary 'weather' with city details
         weather = {
             'city' : city.name, 
             'temperature' : r['main']['temp'],
             'description' : r['weather'][0]['description'],
             'icon' : r['weather'][0]['icon'],
+            'temp_min': r['main']['temp_min'],
+            'temp_max': r['main']['temp_max'],
         }
-
         weather_data.append(weather)
 
     return render_template('weather.html', weather_data=weather_data)
