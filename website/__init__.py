33410cb1eab307d20fa365b0087c9329d023e57d
Jennifer Chhie <22698132+jschhie@users.noreply.github.com>
2025-12-20 21:00:10 -0800

    feat: Flask secret key .env var & update reqs & .gitignore

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from os import path
+
+from os import path, environ
+from dotenv import load_dotenv
 
 db = SQLAlchemy()
 DB_NAME ="weather.db"
@@ -9,7 +11,18 @@ DB_NAME ="weather.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'fb66099b595b09d56b7082aab997b1ee' # to enable sessions and flashed messages
+
+    # Get absolute path of current file (for server and local dev)
+    current_dir = path.abspath(path.dirname(__file__))
+
+    project_root = path.dirname(current_dir)
+    env_path = path.join(project_root, '.env')
+
+    load_dotenv(env_path)
+
+    # Fetch secret key from .env var (Default: placeholder string if DNE)
+    # (To enable sessions and flashed messages)
+    app.config['SECRET_KEY'] = environ.get('FLASK_SECRET_KEY', 'local-dev-only-secret-key')
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
